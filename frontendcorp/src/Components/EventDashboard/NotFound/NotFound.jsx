6121999bb1b542ccc559afa2300441bccf2bf626
jameela786 <jameelashaik7799@gmail.com>
2026-02-14 19:35:32 +0530

    not found updated

--- frontendcorp/src/Components/EventDashboard/NotFound/NotFound.jsx
+++ frontendcorp/src/Components/EventDashboard/NotFound/NotFound.jsx
@@ -1,8 +1,9 @@
+import {Link} from 'react-router-dom'
 import './NotFound.css'
 export const NotFound = () => (
     <div className="not-found">
       <h1>404</h1>
       <p>Page not found</p>
-      <a href="/events">Go to Events</a>
+      <Link to="/events">Go to Events</Link>
     </div>
   );
